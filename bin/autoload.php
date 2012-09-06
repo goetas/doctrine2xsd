ef0fb1865657ca13356ce62133a9ded4f8a7fe11
Asmir Mustafic <goetas@lignano.it>
2012-09-06 14:36:14 +0200

    submodule

--- bin/autoload.php
+++ bin/autoload.php
@@ -1,15 +1,3 @@
 <?php
 
-$symfonyPath = '/www/htdocs/immobinet-asmir/vendor/symfony/src';
-
-require_once $symfonyPath.'/Symfony/Component/ClassLoader/UniversalClassLoader.php'; 
-
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-
-$loader->registerNamespaces(array(
-    'Symfony'          => $symfonyPath,
-	'Goetas\DoctrineToXsd'      => __DIR__.'/../lib',
-));
-
-$loader->register();
 
